208a1d90d6c4a209fe5bb578d5ef1149b2e71190
HOLA_LXL <lixinli.bj@163.com>
2020-05-24 17:06:58 +0800

    第二次课：如何自定义webpack和babel

--- src/main.js
+++ src/main.js
@@ -3,7 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+
+// 为了解决加载没有使用的组件，造成网络压力的问题
+// import { Button, Input } from "ant-design-vue/lib/button";
+// import "ant-design-vue/lib/button/style";
+
+// 如果需要使用多个组件，每个组件写上边的两行代码实在太繁琐
+// 可以借助 Babel 组件，在 babel.config.js 文件中进行配置后，就可以只写一行代码
+import { Button, Input } from "ant-design-vue";
+
 Vue.config.productionTip = false;
+
+// Vue.use(Antd);
+Vue.use(Button);
+Vue.use(Input);
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 选项
+  css: {
+    loaderOptions: {
+      // css: {
+      //     // 这里的选项会传递给 css-loader
+      // },
+      less: {
+        // 这里的选项会传递给 less-loader
+        javascriptEnabled: true
+      }
+      // postcss: {
+      //   // 这里的选项会传递给 postcss-loader
+      // },
+      // sass: {
+      //     // 这里的选项会传递给 sass-loader
+      // },
+      // stylus: {
+      //     // 这里的选项会传递给 stylus-loader
+      // },
+    }
+  }
+}
